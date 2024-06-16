542b69619223f9344c8a8ce2bebc5ae42ca475a8
libewa <libewa.git@icloud.com>
2024-06-16 11:30:09 +0200

    run on multiple systems

--- flake.nix
+++ flake.nix
@@ -3,32 +3,39 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-  };
-
-  outputs = { self, nixpkgs }: {
-    apps.x86_64-linux.default = {
-      type = "app";
-      program = "${self.defaultPackage.x86_64-linux}/bin/makevideo.sh";
-    };
-
-    defaultPackage.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
-      name = "makevideo";
-      src = ./.;
-      buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
-        bash
-        ffmpeg
-        yt-dlp
-      ];
-
-      nativeBuildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
-        makeWrapper
-      ];
-      installPhase = ''
-        mkdir -p $out/bin
-        cp makevideo.sh $out/bin/
-        wrapProgram $out/bin/makevideo.sh \
-          --prefix PATH : "${nixpkgs.legacyPackages.x86_64-linux.lib.makeBinPath [ nixpkgs.legacyPackages.x86_64-linux.ffmpeg nixpkgs.legacyPackages.x86_64-linux.yt-dlp]}"
-      '';
+    flakeutils = {
+      url = "github:numtide/flake-utils";
     };
   };
+
+  outputs = { self, nixpkgs, flakeutils }:
+    flakeutils.lib.eachSystem ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "i686-linux"] (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        apps.default = {
+          type = "app";
+          program = "${self.defaultPackage.${system}}/bin/makevideo.sh";
+        };
+
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          name = "makevideo";
+          src = ./.;
+          buildInputs = with pkgs; [
+            bash
+            ffmpeg
+            yt-dlp
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+          ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp makevideo.sh $out/bin/
+            wrapProgram $out/bin/makevideo.sh \
+              --prefix PATH : "${pkgs.lib.makeBinPath [ pkgs.ffmpeg pkgs.yt-dlp]}"
+          '';
+        };
+      }
+    );
 }
